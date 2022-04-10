2f6d37d46bf0f2bd723fb0db90509dc38d4ac539
Swethasudarsan <swethakadambi.98@gmail.com>
2022-04-10 15:42:58 +0530

    Initial commit

---
+++ src/com/java/mahi/LeftTriangle.java
@@ -0,0 +1,17 @@
+package com.java.mahi;
+
+public class LeftTriangle {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 2 * n - i; j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/com/java/mahi/Pyramid.java
@@ -0,0 +1,8 @@
+package com.java.mahi;
+
+public class Pyramid {
+    public static void Main(String[] args){
+      int n = 5;
+      for(int i =1; i<=n; i++)
+    }
+}
